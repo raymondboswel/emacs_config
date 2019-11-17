d676228d740df1e01a9d69ca1028a7448b5e4302
Raymond Boswel <raymondboswel@gmail.com>
2019-11-17 20:08:15 +0200

    Added golden-ratio package

--- init.el
+++ init.el
@@ -13,10 +13,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit geiser paredit))))
+ '(package-selected-packages (quote (golden-ratio magit geiser paredit))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(require 'golden-ratio)
+
+(golden-ratio-mode 1)
