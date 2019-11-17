d86b60eca749ebccfaba68c8305f38e470b6e156
Raymond Boswel <raymondboswel@gmail.com>
2019-11-17 08:22:58 +0200

    Added emacs init file

---
+++ init.el
@@ -0,0 +1,22 @@
+(require 'package)
+;;; either the stable version:
+
+(add-to-list 'package-archives
+  ;; choose either the stable or the latest git version:
+  '("melpa-stable" . "http://stable.melpa.org/packages/"))
+  ;;'("melpa-unstable" . "http://melpa.org/packages/"))
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit geiser paredit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
